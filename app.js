36dbddd30d6913e702ad858113a40682ba67c10c
unknown <fabio_perelli@yahoo.it>
2023-05-19 13:18:28 +0100

    chatbot for netlify

---
+++ app.js
@@ -0,0 +1,75 @@
+//DOM
+const form = document.querySelector('form');
+const testo = document.getElementById('testo');
+const conversazione = document.getElementById('conversazione');
+const textArea = document.querySelector('textArea');
+const newConversation = document.getElementById('nuova-conversazione');
+
+let conversationArray = [{
+    role: 'system',
+    content: 'sei un assistente con un grande sense of humour che risponde sempre con scherzi e battute divertenti'
+}];
+
+//EVENTS
+form.addEventListener('submit',async (e)=>{
+    e.preventDefault();
+    let conversationObj = {
+        role: 'user',
+        content: testo.value
+    };
+    conversationArray.push(conversationObj);
+    let chatRequest = await fetch('/.netlify/functions/chat');
+    let chatResponse = await chatRequest.json();
+    conversationArray.push(chatResponse);
+    
+    renderDomanda(testo.value);
+    appendDivRisposta();
+    scrollToLastResponse();
+    renderTestoRisposta(chatResponse.content);
+    testo.value = '';
+        
+});
+
+
+newConversation.addEventListener('click',() => {
+    window.location.reload();
+});
+
+//Util functions
+
+function renderDomanda(testo){
+    let div = document.createElement('div');
+    div.classList.add('domanda');
+    div.textContent = testo;
+    conversazione.append(div);
+}
+
+function appendDivRisposta(){
+    let div = document.createElement('div');
+    div.classList.add('risposta');
+    conversazione.append(div);
+}
+
+function scrollToLastResponse(){
+    const lastResponse = document.querySelector('div.risposta:last-of-type');
+    lastResponse.scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"}); 
+}
+
+function renderTestoRisposta(testo){
+    const lastResponse = document.querySelector('div.risposta:last-of-type');
+    let index = 0;
+    
+    function typeWriting(){
+        if(index < testo.length){
+            lastResponse.textContent += testo.charAt(index);
+            index++;
+            scrollToLastResponse();
+            setTimeout(typeWriting,50);
+        }
+        else{
+            textArea.scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"});
+        };
+    }
+
+    typeWriting();
+}
---
+++ netlify/functions/chat.js
@@ -0,0 +1,30 @@
+const AI = {
+    URL: 'https://api.openai.com/v1/chat/completions',
+    headers: {
+        "Content-type": "application/json",
+        "Authorization": "Bearer "+API_KEY
+    },
+    chat: async function(conversationArray){
+        let response = await fetch(this.URL,{
+            method: "POST",
+            headers: this.headers,
+            body: JSON.stringify({
+                "model": "gpt-3.5-turbo",
+                "messages": conversationArray,
+                "presence_penalty": 0,
+                "frequency_penalty": 0.3     
+              })
+        });
+        let data = await response.json();
+        return data.choices[0].message;
+    }
+}
+
+export const handler = async (event) => {
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: AI.chat(event.body),
+      }),
+    }
+  }
